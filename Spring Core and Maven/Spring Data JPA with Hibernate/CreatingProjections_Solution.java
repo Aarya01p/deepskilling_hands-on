2631a91fa92696aa28b7625012959651282d8b8a
Aarya Patel <aaryap066@gmail.com>
2026-07-10 20:33:42 +0530

    Delete Spring Core and Maven directory

--- Spring Core and Maven/Spring Data JPA with Hibernate/CreatingProjections_Solution.java
+++
@@ -1,9 +0,0 @@
-/*
-QUESTION:
-Employee Management System - Creating Projections
-Define interface-based projections for Employee entity to fetch limited fields.
-*/
-public interface EmployeeProjection {
-    String getName();
-    String getEmail();
-}
